1f7f5c0cdcae6dc5b7087171f35989f85e0febbb
christian99salerno <christian.salerno05@gmail.com>
2017-03-27 18:40:28 +0200

    Problemi nel disegno

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ screen=curses.initscr()
 
 curses.curs_set(0)
 
-scacchiera1=ScacchieraNC(5, 5)
+scacchiera1=ScacchieraNC(screen, 5, 5)
 
-scacchiera1.stampa(screen, 5, 5)
+scacchiera1.stampa(5, 5)
 
 screen.refresh()
 
--- scacchiera_nc.py
+++ scacchiera_nc.py
@@ -2,11 +2,12 @@ from scacchiera import Scacchiera
 import curses
 
 class ScacchieraNC(Scacchiera):
-    def __init__(self, r, c):
+    def __init__(self, screen, r, c):
         Scacchiera.__init__(self, r, c)
-    
-    def stampa(self, screen, rig, col):
-        for i in range (0,self.r*4,4):
-            self.screen.addstr(rig,col+i,"-"*(self.r*4))
+        self.screen = screen
 
+    def stampa(self, rig, col):
+        for i in range (0,self.r*4+1,4):
+            self.screen.addstr(rig+i,col,"-"*(self.r*4))
+    
             
