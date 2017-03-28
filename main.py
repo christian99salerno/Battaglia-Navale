2d52a45c1e92036b695648031cf8e154b49825c6
alunno <alunno@debian04>
2017-03-28 10:01:00 +0200

    Disegno completato

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ screen=curses.initscr()
 curses.curs_set(0)
 
 scacchiera1=ScacchieraNC(screen, 5, 5)
+scacchiera2=ScacchieraNC(screen, 5, 5)
 
+scacchiera3=ScacchieraNC(screen, 5, 5)
+scacchiera4=ScacchieraNC(screen, 5, 5)
+
+#Giocatore1
 scacchiera1.stampa(5, 5)
+scacchiera2.stampa(5, 30)
+
+#Giocatore2
+scacchiera3.stampa(5, 65)
+scacchiera4.stampa(5, 90)
 
 screen.refresh()
 
--- scacchiera_nc.py
+++ scacchiera_nc.py
@@ -7,7 +7,12 @@ class ScacchieraNC(Scacchiera):
         self.screen = screen
 
     def stampa(self, rig, col):
-        for i in range (0,self.r*4+1,4):
-            self.screen.addstr(rig+i,col,"-"*(self.r*4))
-    
-            
+	for i in range(0,self.r*4+1,4):
+		self.screen.addstr(rig+i,col,"-"*(self.r*4))
+        for i in range(0,self.c*4+1,4):
+            for j in range(0,self.c*4+1,1):
+                self.screen.addstr(rig+j,col+i,"|")   
+        for i in range(0,self.c*4+1,4):
+            for j in range(0,self.c*4+1,4):
+                self.screen.addstr(rig+j,col+i,"+")   
+
