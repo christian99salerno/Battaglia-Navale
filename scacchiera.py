c9ab474634213dcd6825e1c701e7ac835f7e4620
alunno <alunno@debian04>
2017-04-03 14:08:33 +0200

    modifiche

--- scacchiera.py
+++ scacchiera.py
@@ -3,13 +3,14 @@ class Scacchiera:
         self.r=r
         self.c=c
         self.posizioni=[]
+        self.pos_nn_accet=[]
+
     def posizione_nave(self,posizione):
         if(posizione[0] > self.r or posizione[0] < 0 or posizione[1] > self.c or posizione[1] < 0):
             return
+        if posizione[0
         else:
             self.posizioni.append(posizione)
-    
-                        
-        
-    
+        self.pos_nn_accet.append((posizione[0],posizione[1]),(posizione[0],posizione[1]-1),(posizione[0],posizione[1]+1),(posizione[0]-1,posizione[1]),(posizione[0]-1,posizione[1]+1),(posizione[0]-1,posizione[1]-1),(posizione[0]+1,posizione[1]),(posizione[0]+1,posizione[1]-1),(posizione[0]+1,posizione[1]+1))
+
 
--- scacchiera_nc.py
+++ scacchiera_nc.py
@@ -11,25 +11,23 @@ class ScacchieraNC(Scacchiera):
     def stampa(self):
 	for i in range(0,self.r*4+1,4):
 		self.screen.addstr(self.rig+i,self.col,"-"*(self.c*4))
+        
         for i in range(0,self.c*4+1,4):
             for j in range(0,self.r*4+1,1):
                 self.screen.addstr(self.rig+j,self.col+i,"|")   
+        
         for i in range(0,self.r*4+1,4):
             for j in range(0,self.c*4+1,4):
                 self.screen.addstr(self.rig+i,self.col+j,"+")   
-
-      # for i in range(2,self.r*4,4):
-          # for j in range(2,self.c*4,4):
-              # self.screen.addstr(rig+i,col+j,"x")
         
-        for i in range(1,self.r*4,4):
-            self.screen.addstr(self.rig+i+1,self.col-1,str(i/4))
+        for righe in range(1,self.r+1):
+            self.screen.addstr(self.rig+2+4*(righe-1), self.col-1,  str(righe))
         
-        for j in range(1,self.c*4,4):
-            self.screen.addstr(self.rig-1,self.col+j+1,str(j/4)) 
-
+        for colonne in range(1, self.c+1):
+            self.screen.addstr(self.rig-1, self.col+2+4*(colonne-1), str(colonne))
+        
     def stampa_navi(self):
         for posizione in self.posizioni: 
-            self.screen.addstr(posizione[0]+self.rig+2*4,posizione[1]+self.col+2*4,"x")
+            self.screen.addstr((self.rig+2)+4*(posizione[0]-1),(self.col+2)+4*(posizione[1]-1),"x")
         
 
