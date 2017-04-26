6d7a7846c523eefd55181f2eb2cb16eda872ad64
alunno <alunno@debian04>
2017-04-26 11:23:19 +0200

    Verso la fine del progetto; curare la parte del main

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ curses.curs_set(0)
 
 #Giocatore1
 
+screen.addstr(1,5,"Giocatore:1")
+
 scacchiera1=ScacchieraNavi(5, 5)
 
 scacchiera1.posizione_nave((4,4), 2, 'h')
 scacchiera1.posizione_nave((1,1), 2, 'v')
+scacchiera1.posizione_nave((1,3), 1, "v")
+scacchiera1.posizione_nave((5,1), 1, 'v')
+scacchiera1.posizione_nave((2,5), 1, 'v')
 
 scacchiera2=ScacchieraColpi(5, 5)
 
@@ -25,23 +30,30 @@ scacchiera2=ScacchieraColpi(5, 5)
 
 #Giocatore2
 
+screen.addstr(1,140,"Giocatore:2")
+
 scacchiera3=ScacchieraColpi(5, 5)
 
 scacchiera4=ScacchieraNavi(5, 5)
 
 scacchiera4.posizione_nave((4,3), 1, "h")
 scacchiera4.posizione_nave((2,2), 2, "h")
-#scacchiera4.BreakShip((4,3))
-#scacchiera2.posizione_colpi((4,3))
-#scacchiera4.BreakShip((2,3))
-#scacchiera2.posizione_colpi((2,3))
+
 legami(scacchiera2, scacchiera4, (4,3))
+legami(scacchiera3, scacchiera1, (2,2))
+legami(scacchiera3, scacchiera1, (1,1))
+legami(scacchiera3, scacchiera1, (2,1))
+legami(scacchiera3, scacchiera1, (1,3))
+legami(scacchiera3, scacchiera1, (2,5))
+legami(scacchiera3, scacchiera1, (4,4))
+legami(scacchiera3, scacchiera1, (4,5))
+legami(scacchiera3, scacchiera1, (5,1))
 
 StampaScacchiera(scacchiera1, screen, 5, 5)
-StampaScacchiera(scacchiera4, screen, 5, 160)
+StampaScacchiera(scacchiera4, screen, 5, 165)
 
 StampaScacchiera(scacchiera2, screen, 5, 30) 
-StampaScacchiera(scacchiera3, screen, 5, 135)
+StampaScacchiera(scacchiera3, screen, 5, 140)
 
 screen.refresh()
 
--- scacchiera_navi.py
+++ scacchiera_navi.py
@@ -1,14 +1,20 @@
 from scacchiera_generale import *
 
+maxnavi=7
+
 class ScacchieraNavi(ScacchieraGenerale):
     def __init__(self,r,c):
         ScacchieraGenerale.__init__(self,r,c)
         self.pos_nn_accet=[]
         self.navi_abbattute=[]
+        self.cbreak=0
+        self.navi1max=3
+        self.navi2max=2
 
     def posizione_nave(self,posizione,grande,verso):
+        
         if grande==1:
-            if posizione[0]>self.r or posizione[0]<0 or posizione[1]>self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet:
+            if posizione[0]>self.r or posizione[0]<0 or posizione[1]>self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet or self.navi1max==0:
                 return
             else:
                 self.posizioni.append(posizione)
@@ -21,9 +27,10 @@ class ScacchieraNavi(ScacchieraGenerale):
                 self.pos_nn_accet.append((posizione[0]+1,posizione[1]))
                 self.pos_nn_accet.append((posizione[0]+1,posizione[1]-1))
                 self.pos_nn_accet.append((posizione[0]+1,posizione[1]+1))
+                self.navi1max-=1
 
         elif grande==2 and verso=="h":
-            if posizione[0]>self.r or posizione[0]<0 or posizione[1]>=self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet or (posizione[0],posizione[1]+1) in self.pos_nn_accet:
+            if posizione[0]>self.r or posizione[0]<0 or posizione[1]>=self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet or (posizione[0],posizione[1]+1) in self.pos_nn_accet or self.navi2max==0:
                 return
             else:
                 self.posizioni.append(posizione)
@@ -40,9 +47,10 @@ class ScacchieraNavi(ScacchieraGenerale):
                 self.pos_nn_accet.append((posizione[0],posizione[1]+2))
                 self.pos_nn_accet.append((posizione[0]+1,posizione[1]+2))
                 self.pos_nn_accet.append((posizione[0]-1,posizione[1]+2))
+                self.navi2max-=1
 
         elif grande==2 and verso=="v":
-            if posizione[0]>=self.r or posizione[0]<0 or posizione[1]>self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet or (posizione[0]+1,posizione[1]) in self.pos_nn_accet:
+            if posizione[0]>=self.r or posizione[0]<0 or posizione[1]>self.c or posizione[1]<0 or (posizione[0], posizione[1]) in self.pos_nn_accet or (posizione[0]+1,posizione[1]) in self.pos_nn_accet or self.navi2max==0:
                 return
             else:
                 self.posizioni.append(posizione)
@@ -59,12 +67,17 @@ class ScacchieraNavi(ScacchieraGenerale):
                 self.pos_nn_accet.append((posizione[0]-2,posizione[1]-1))
                 self.pos_nn_accet.append((posizione[0]-2,posizione[1]))
                 self.pos_nn_accet.append((posizione[0]-2,posizione[1]+1))
-    
+                self.navi2max-=1
+
     def BreakShip(self, colpo):
-        if colpo in self.posizioni:
-            self.navi_abbattute.append(colpo)
-        else:
+        if self.cbreak==maxnavi:
             return
+        else:
+            if colpo in self.posizioni:
+               self.navi_abbattute.append(colpo)
+               self.cbreak=self.c+1
+            else:                   
+               return
              
 
 
